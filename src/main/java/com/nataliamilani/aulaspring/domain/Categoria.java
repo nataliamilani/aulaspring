4f62043ab37fa4ea5ff0d7399b338e0a364bbdd5
Natalia Milani <natalia.milani@hotmail.com>
2022-07-25 17:42:05 -0300

    Configuracoes do banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/nataliamilani/aulaspring/domain/Categoria.java
+++ src/main/java/com/nataliamilani/aulaspring/domain/Categoria.java
@@ -1,12 +1,21 @@
 package com.nataliamilani.aulaspring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
